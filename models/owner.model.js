217d2a24988e157297b9f3612f2eb5695c53a0fa
HashGen <abechuphojaoff@gmail.com>
2025-09-30 11:23:41 +0530

    Update owner.model.js

--- models/owner.model.js
+++ models/owner.model.js
@@ -5,6 +5,9 @@ const ownerSchema = new mongoose.Schema({
     first_name: String,
     wallet_balance: { type: Number, default: 0 },
     total_earnings: { type: Number, default: 0 },
-    created_at: { type: Date, default: Date.now }
+    created_at: { type: Date, default: Date.now },
+    language: { type: String, default: 'en' },
+    is_banned: { type: Boolean, default: false },
+    banned_at: { type: Date }
 });
 module.exports = mongoose.model('Owner', ownerSchema);
